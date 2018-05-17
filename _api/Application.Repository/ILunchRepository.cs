092473d4f027b8c92197051551cc771cd8d21dcb
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2018-05-17 09:05:27 -0500

    JB - Add database files [store procedures, repositoryfactory]

--- _api/Application.Repository/ILunchRepository.cs
+++ _api/Application.Repository/ILunchRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Application.Entities;
+using Insight.Database;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,19 @@ namespace Application.Repository
 {
     public interface ILunchRepository
     {
+        [Sql("SP_CREATE_LUNCH", CommandType.StoredProcedure)]
+        Lunch CreateLunch (Lunch lunch);
+
+        [Sql("SP_DELETE_LUNCH", CommandType.StoredProcedure)]
+        Lunch DeleteLunch (Guid lunchID);
+
+        [Sql("SP_UPDATE_LUNCH", CommandType.StoredProcedure)]
+        Lunch UpdateLunch (Guid lunchID, Lunch lunch);
+
+        [Sql("SP_GETBYID_LUNCH", CommandType.StoredProcedure)]
+        Lunch GetByIdLunch (Lunch body);
+
+        [Sql("SP_GET_LUNCH", CommandType.StoredProcedure)]
+        List<Lunch> GetLunch (Lunch body);
     }
 }
--- _api/Application.Repository/impl/LunchRepository.cs
+++ _api/Application.Repository/impl/LunchRepository.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Entities;
 
 namespace Application.Repository.impl
 {
-    public class LunchRepository
+    public class LunchRepository : ILunchRepository
     {
+        public Lunch CreateLunch(Lunch lunch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lunch DeleteLunch(Guid lunchID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lunch GetByIdLunch(Lunch body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Lunch> GetLunch(Lunch body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lunch UpdateLunch(Guid lunchID, Lunch lunch)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
