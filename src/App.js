0b23e2de7bcac452461b530d638ad4b6e80f2994
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2018-05-29 13:39:17 -0500

    JB - Change frontend data - React.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      launchList: []
+    }
+  }
+
+  componentDidMount() {
+    this.getLunchList();
+  }
+
+  getLunchList() {
+    fetch('http://40.114.38.57:9700/_api/Application.WebApi/api/launch')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ launchList: res })
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +31,31 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.state.launchList.map(data => {
+
+            const ingredients = data.ingredient.split(';')
+            const images = data.image.split(';')
+
+            return <div key={data._id}>
+              <h1>{data.name}</h1>
+              <p>{data.description}</p>
+              <h4>Ingredients</h4>
+              {
+                ingredients.map( (ingredient,i) => {
+                  return <ul key={i}>
+                    <li >{ingredient}</li>
+                  </ul>
+                })
+              }
+              {
+                images.map( (images,i) => {
+                  return <img key={i} src={images} height="300" width="300"/>
+                })
+              }
+            </div>
+          })
+        }
       </div>
     );
   }
