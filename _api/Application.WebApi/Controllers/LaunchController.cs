549c53be10787e39a058643f15ebcd64207599a9
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2018-06-01 13:57:53 -0500

    FIX: Update [Route("...")]

--- _api/Application.WebApi/Controllers/LaunchController.cs
+++ _api/Application.WebApi/Controllers/LaunchController.cs
@@ -11,26 +11,26 @@ using System.Web.Http.Cors;
 
 namespace Application.WebApi.Controllers
 {
-    [RoutePrefix("api")]
+    [RoutePrefix("api/launch")]
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class LaunchController : ApiController
     {
 
         private static LunchRepository repository = RepositoryFactory.GetLunchRepository();
-
-        [HttpPost, Route("launch")]
-        public IHttpActionResult CreateLunch([FromBody]Lunch lunch) => Ok(repository.CreateLunch(lunch));
         
-        [HttpDelete, Route("launch/{lunchID}")]
-        public IHttpActionResult DeleteLunch(string lunchID) => Ok(repository.DeleteLunch(Guid.Parse(lunchID)));
-        
-        [HttpGet, Route("launch/{lunchID}")]
-        public IHttpActionResult GetByIdLunch(string lunchID) => Ok(repository.GetByIdLunch(Guid.Parse(lunchID)));
-        
-        [HttpGet, Route("launch")]
+        [HttpGet]
         public IHttpActionResult GetLunch(int? PageNumber = 1, int? PageSize = 5) => Ok(repository.GetLunch(PageNumber, PageSize));
         
-        [HttpPut, Route("launch/{ID}")]
+        [HttpPost]
+        public IHttpActionResult CreateLunch([FromBody]Lunch lunch) => Ok(repository.CreateLunch(lunch));
+        
+        [HttpDelete, Route("{lunchID}")]
+        public IHttpActionResult DeleteLunch(string lunchID) => Ok(repository.DeleteLunch(Guid.Parse(lunchID)));
+        
+        [HttpGet, Route("{lunchID}")]
+        public IHttpActionResult GetByIdLunch(string lunchID) => Ok(repository.GetByIdLunch(Guid.Parse(lunchID)));
+               
+        [HttpPut, Route("{ID}")]
         public IHttpActionResult UpdateLunch(string ID, [FromBody]Lunch lunch) => Ok(repository.UpdateLunch(Guid.Parse(ID), lunch));
         
     }
